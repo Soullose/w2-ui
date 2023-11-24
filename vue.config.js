9e8217cc20bbeefac3aedbd8727760df4ae1f9d9
WangSF <soullose@qq.com>
2023-11-24 15:32:58 +0800

    update 20231124-1523

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,19 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+function resolve(dir) {
+    return path.join(__dirname, dir);
+}
 module.exports = defineConfig({
     transpileDependencies: ['vuetify'],
     outputDir: 'dist',
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@': resolve('src'),
+                '~': resolve('src/')
+            }
+        }
+    },
     devServer: {
         port: 8888,
         hot: true,
